b8cd2c8e8ea804bcd5df8b3c8a5ab3269961e7a8
pratikm <pratik.manandhar99@gmail.com>
2025-09-04 15:25:28 +0200

    add posts component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { redditReducer } from './store/reddit.reducer';
+import { RedditEffects } from './store/reddit.effect';
+import { provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
+    importProvidersFrom([CommonModule]),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideClientHydration(withEventReplay()),
+    provideStore({reddit: redditReducer}),
+    provideEffects([RedditEffects]),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Posts } from './posts/posts';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { redditReducer } from './store/reddit.reducer';
+import { RedditEffects } from './store/reddit.effect';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Posts, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('reddit-client');
